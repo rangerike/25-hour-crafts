e8d3d2df40663490bba34a82a5e6858eb6db058e
Scott Bitz <SBitz@C-K.com>
2017-10-24 09:54:45 -0500

    - Minor text updates

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -1,14 +1,16 @@
 <main id="content" role="main">
   <h1 class="m3">Contact Us</h1>
   <article class="photo-article">
-    <h2 class="m3 italics bold h3">For Custom Orders and possible vending opportunities:</h2>
     <p class="m3">
-      We're always happy to hear from you whatever the reason may be:
+      If you're looking to get in touch with us about a possible vending opportunity or you're interested in
+      something that we make, you can reach us at <a href="mailto:craftyhobbitshop@gmail.com">craftyhobbitshop@gmail.com</a>
+      and we'll get back to you as soon as we can.
     </p>
     <p class="m3">
-      <a href="mailto:craftyhobbitshop@gmail.com">Vending and Orders</a>
+      However, if you want to reach out to Kayla or Scott specifically about anything relating to our crafts,
+      you can reach us by the email addresses below:
     </p>
-    <a href="mailto:sbitz@25thhourcrafts.com"><h2 class="m3">Scott Bitz</h2></a>
-    <a href="mailto:kbitz@25thhourcrafts.com"><h2 class="m3">Kayla Bitz</h2></a>
+    <a href="mailto:kbitz@25thhourcrafts.com"><h2 class="m3">kbitz@25thhourcrafts.com - Kayla Bitz</h2></a>
+    <a href="mailto:sbitz@25thhourcrafts.com"><h2 class="m3">sbitz@25thhourcrafts.com - Scott Bitz</h2></a>
   </article>
 </main>
