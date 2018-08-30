17e0614c67d148841ba731f686ea355e4888a6bf
rangerike <bitzsc@gmail.com>
2018-08-29 20:57:28 -0500

    - Add second set of landing images

--- application/views/landing.php
+++ application/views/landing.php
@@ -10,8 +10,8 @@
       </amp-img>
       <div class="commerce-hero-content commerce-hero-content-theme-primary flex justify-center flex-column md-mx-auto md-p0">
         <div class="commerce-hero-align md-mx-auto">
-          <h2 class="commerce-hero-content-title h4 md-h1 caps md-mb2 md-pl4">Dicebags</h2>
-          <p class="commerce-hero-content-body black-shadow">These hold your precious dice that every RPGer needs to get their game on!</p>
+          <h2 class="commerce-hero-content-title h4 md-h1 caps md-mb2 md-pl4">Dice Bags</h2>
+          <p class="commerce-hero-content-body">Licensed and Novelty cotton dice bags lined with soft polyester satin that every RPGer needs to get their game on!</p>
           <div class="ampstart-btn ampstart-btn-secondary caps md-ml4">shop</div>
         </div>
       </div>
@@ -19,16 +19,16 @@
   </section>
   <section class="commerce-hero-content-wrapper center overflow-hidden">
     <a href="blog-listing.amp.html" class="text-decoration-none block">
-      <amp-img class="commerce-hero-image" src="../img/e-commerce/narrow-landing-02.jpg" width="750" height="573" layout="responsive" media="(max-width: 52.06rem)" alt="Hero Landing 1" noloading="">
+      <amp-img class="commerce-hero-image" src="<?=asset_url()?>images/geekkon12-madison-narrow.jpg" width="750" height="573" layout="responsive" media="(max-width: 52.06rem)" alt="Hero Landing 1" noloading="">
         <div placeholder="" class="commerce-loader"></div>
       </amp-img>
-      <amp-img class="commerce-hero-image" src="../img/e-commerce/wide-landing-02.jpg" width="2560" height="800" layout="responsive" media="(min-width: 52.07rem)" alt="Hero Landing 1" noloading="">
+      <amp-img class="commerce-hero-image" src="<?=asset_url()?>images/geekkon12-madison-wide.jpg" width="2560" height="800" layout="responsive" media="(min-width: 52.07rem)" alt="Hero Landing 1" noloading="">
         <div placeholder="" class="commerce-loader"></div>
       </amp-img>
       <div class="commerce-hero-content commerce-hero-content-theme-primary flex justify-center flex-column md-mx-auto md-p0">
         <div class="commerce-hero-align md-mx-auto">
-          <h2 class="commerce-hero-content-title h4 md-h1 caps md-mb2 md-pl4">Lost Highways</h2>
-          <p class="commerce-hero-content-body">Go the distance and discover new levels of endurance along the way. Explore the world’s 10 toughest randonnées.</p>
+          <h2 class="commerce-hero-content-title h4 md-h1 caps md-mb2 md-pl4">Displays</h2>
+          <p class="commerce-hero-content-body">Learn about the importance of great displays and how they increase your sales.</p>
           <div class="ampstart-btn ampstart-btn-secondary caps md-ml4">read</div>
         </div>
       </div>
