6c75a12974332b228c2ad22fe6cfe33292f9d780
Scott Bitz <SBitz@C-K.com>
2018-09-06 16:13:13 -0500

    - Updates the product list to appear on the /shop page instead of shop/product - Fixes the cart and checkout examples so they're more useful

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -1,10 +1,23 @@
 <?php
 class Shop extends CI_Controller {
 
-	public function index()
-	{}
+	public function index($page = 'products')
+	{
+		$data = array(
+			'page' => ucfirst($page),
+			'canonicalURI' => 'shop/',
+			'amp_list' => TRUE,
+			'amp_bind' => TRUE,
+			'amp_mustache' => TRUE,
+			'meta_description' =>''
+		);
 
-  public function product($page = 'product_list')
+		$this->load->view('templates/header', $data);
+    $this->load->view('shop/product_list', $data);
+    $this->load->view('templates/footer', $data);
+	}
+
+  public function product($productID)
   {
 
 		$data = array(
@@ -12,7 +25,8 @@ class Shop extends CI_Controller {
 			'canonicalURI' => 'shop/product'.($page==='product_list'?'':'/'.$page),
 			'amp_list' => TRUE,
 			'amp_bind' => TRUE,
-			'amp_mustache' => TRUE
+			'amp_mustache' => TRUE,
+			'meta_description' =>''
 		);
 
     $this->load->view('templates/header', $data);
